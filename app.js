be11358e39eaf663ae923a4d5e8b72bec23cf2eb
Carole Gérard <gerard.carole@skynet.be>
2022-07-29 15:14:17 +0200

    librairie qui gère les erreurs async await + connection à la bdd

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 // module qui permet de gérer (d'aller rechercher) les variables d'environnement
 require('dotenv-flow').config();
 
+
 // Création du serveur Express
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 
 // variables d'environnement
-const { NODE_ENV, MESSAGE, PORT } = process.env;
+const { NODE_ENV, MESSAGE, PORT, DB_CONNECTION } = process.env;
 console.log('Lancé en', NODE_ENV, ':', MESSAGE);
 
 // route temporaire
@@ -19,11 +21,30 @@ console.log('Lancé en', NODE_ENV, ':', MESSAGE);
 // });
 
 
+// on importe la librairie qui gère les erreurs async await 
+// ATTENTION : à placer avant d'importer les routes !
+require('express-async-errors');
+
+
 // on importe notre module router
 const router = require('./routes');
 
+
+// middleware qui permet au serveur de lire des objets json en post
+app.use(express.json());
+
+
+// connection à la bdd
+app.use( async (req, res, next) => {
+    await mongoose.connect(DB_CONNECTION);
+    console.log('Connection réussie');
+    next();
+})
+
+
 // on indique à notre serveur qu'une fois arrivé sur la route /api, il doit utiliser notre router
 app.use('/api', router);
+
 
 // on met le serveur sur écoute
 app.listen(PORT, () => {
