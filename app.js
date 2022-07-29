252ffb077cd0c016d8de8eaddb0db7e5ff2a1b69
Carole Gérard <gerard.carole@skynet.be>
2022-07-29 13:28:29 +0200

    route temporaire

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-
-const express = require('express');
-
-const app = express();
-
+// module qui permet de gérer (d'aller rechercher) les variables d'environnement
 require('dotenv-flow').config();
 
-const { NODE_ENV, MESSAGE } = process.env;
+// Création du serveur Express
+const express = require('express');
+const app = express();
+
+// variables d'environnement
+const { NODE_ENV, MESSAGE, PORT } = process.env;
 console.log('Lancé en', NODE_ENV, ':', MESSAGE);
+
+// route temporaire
+app.use('/projet', (req, res) => {
+    const data = {
+        msg : 'Projet Burger'
+    };
+    res.json(data);
+});
+
+// on met le serveur sur écoute
+app.listen(PORT, () => {
+    console.log(`Server up on port : ${PORT} [${NODE_ENV}]`);
+});
