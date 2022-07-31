997660722c027ff4486f4c6890a2835f8a23b3d0
Carole Gérard <gerard.carole@skynet.be>
2022-07-31 17:01:00 +0200

    fonctions dans le fichier user-controller.js (+ dto)

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,9 @@
+// TODO: userDTO ou .select() ??
+// const userDTO = require("../dto/user-dto");
 const User = require("../models/user-model");
+
+// TODO: userDTO ou .select() ??
+// const userMapperToDTO = user => new userDTO(user.id, user.firstname, user.lastname, user.email, user.adress);
 
 // fonctions appelées pour chaque routes
 const userController = {
@@ -7,31 +12,58 @@ const userController = {
     // j'ajoute temporairement la fonction "create" qui permet de créer quelques "users" pour tester dans insomnia, en attendant de faire le "register" 
     create : async (req, res) => {
         const userToAdd = User(req.body);
-        // console.log(req.body);
-        // console.log(userToAdd);
         await userToAdd.save();
         res.status(200).json(userToAdd);
     },
     // ----------------------------------------------------------------------------
+
     getAll : async (req, res) => {
-        const users = await User.find().select({ firstname : 1, lastname : 1, eamil : 1, adress : 1});
+
+        const users = await User.find()
+        .select({ firstname : 1, lastname : 1, email : 1, adress : 1});
         res.status(200).json(users);
+
     },
+
     getById : async (req, res) => {
+
         const id = req.params.id;
-        const user = await User.findById(id);
+        const user = await User.findById(id)
+        .select({ firstname : 1, lastname : 1, email : 1, adress : 1});
         if (!user) {
-            res.sendStatus(404);
+            return res.sendStatus(404);
         }
         res.status(200).json(user);
+
     },
-    update : (req, res) => {
-        console.log(`Modification du user dont l\'id est [${req.params.id}]`);
-        res.sendStatus(501);
+
+    update : async (req, res) => {
+
+        const id = req.params.id;
+        const { firstname, lastname, email, adress } = req.body;
+        const userToUpdate = await User.findByIdAndUpdate(id, {
+            firstname,
+            lastname,
+            email,
+            adress
+        }, 
+        { returnDocument : 'after' })
+        .select({ firstname : 1, lastname : 1, email : 1, adress : 1});
+        if (!userToUpdate) {
+            return res.sendStatus(404);
+        }
+        res.status(200).json(userToUpdate);
+
     },
-    delete : (req, res) => {
-        console.log(`Suppression du user dont l\'id est [${req.params.id}]`);
-        res.sendStatus(501);
+
+    delete : async (req, res) => {
+
+        const id = req.params.id;
+        const userToDelete = await User.findByIdAndDelete(id);
+        if (!userToDelete) {
+            return res.sendStatus(404);
+        }
+        res.sendStatus(204);
     }
 };
 
---
+++ dto/user-dto.js
@@ -0,0 +1,14 @@
+// TODO: décommenter ou supprimer
+
+// class userDTO {
+
+//     constructor (id, firstname, lastname, email, adress) {
+//         this.id = id;
+//         this.firstname = firstname;
+//         this.lastname = lastname;
+//         this.email = email;
+//         this.adress = adress;
+//     }
+// }
+
+// module.exports = userDTO;
